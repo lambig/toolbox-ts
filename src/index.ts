89cd5f440a072a51e918c1aeb3ae9843120052d9
Lambig <ceolnua51@gmail.com>
2022-06-17 17:31:12 +0900

    support varargs / added notAll & none

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,49 @@
  */
 export function notNull<E>(target: E | null): target is E { return target !== null; }
 
+type ArrayLengthMutationKeys = 'splice' | 'push' | 'pop' | 'shift' | 'unshift' | number
+type ArrayItems<T extends Array<any>> = T extends Array<infer TItems> ? TItems : never
 /**
- * 
- * @param propositionOrBooleans 
- * @returns if contains no [return of proposition] or [boolean] that is false
+ * Array with (initially) fixed length
+ * big thanks to https://stackoverflow.com/a/59906630
  */
-export const all = (propositionOrBooleans: Array<(() => boolean) | boolean>): boolean =>
-    propositionOrBooleans
-        .every((propositionOrBoolean: (() => boolean) | boolean) =>
-            typeof propositionOrBoolean === "boolean"
-                ? propositionOrBoolean
-                : propositionOrBoolean());
+export type FixedLengthArray<T extends any[]> =
+    Pick<T, Exclude<keyof T, ArrayLengthMutationKeys>>
+    & { [Symbol.iterator]: () => IterableIterator<ArrayItems<T>> }
+
+export type ProPositionOrBoolean = (() => boolean) | boolean;
+const isProPositionOrBoolean = (arg: unknown): arg is ProPositionOrBoolean => (typeof arg === "boolean" || typeof arg === "function");
+
+const truthy = (propositionOrBoolean: ProPositionOrBoolean) => typeof propositionOrBoolean === "boolean" ? propositionOrBoolean : propositionOrBoolean();
+const propositionsOrBooleans = (args: ProPositionOrBoolean[] | FixedLengthArray<[ProPositionOrBoolean[]]>): ProPositionOrBoolean[] =>
+    Array.isArray(args) && args.every(isProPositionOrBoolean)
+        ? args
+        : args[0];
 
 /**
  * 
- * @param propositionOrBooleans 
+ * @param args 
+ * @returns if contains no [return of proposition] or [boolean] that is false
+ */
+export const all = (...args: ProPositionOrBoolean[] | FixedLengthArray<[ProPositionOrBoolean[]]>): boolean => propositionsOrBooleans(args).every(truthy);
+
+/**
+ * 
+ * @param args 
  * @returns if contains any [return of proposition] or [boolean] that is true
  */
-export const any = (propositionOrBooleans: Array<(() => boolean) | boolean>): boolean =>
-    propositionOrBooleans
-        .find((propositionOrBoolean: (() => boolean) | boolean) =>
-            typeof propositionOrBoolean === "boolean"
-                ? propositionOrBoolean
-                : propositionOrBoolean()) !== undefined;
+export const any = (...args: ProPositionOrBoolean[] | FixedLengthArray<[ProPositionOrBoolean[]]>): boolean => propositionsOrBooleans(args).find(truthy) as boolean;
+
+/**
+ * 
+ * @param args 
+ * @returns if contains no [return of proposition] or [boolean] that is true
+ */
+export const none = (...args: ProPositionOrBoolean[] | FixedLengthArray<[ProPositionOrBoolean[]]>): boolean => !any(...args);
+
+/**
+ * 
+ * @param args 
+ * @returns if contains any [return of proposition] or [boolean] that is false
+ */
+export const notAll = (...args: ProPositionOrBoolean[] | FixedLengthArray<[ProPositionOrBoolean[]]>): boolean => !all(...args);
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { notNull, all, any } from "../src/index"
+import { notNull, all, any, notAll, none } from "../src/index"
 
 
 describe(
@@ -11,7 +11,7 @@ describe(
 )
 
 describe(
-    "all",
+    "all_array",
     () => {
         describe(
             "boolean",
@@ -40,16 +40,72 @@ describe(
                 test("all false", () => expect(all([() => false, 1 > 5])).toBeFalsy())
             }
         )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    all([
+                        true,
+                        false,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ]))
+                    .toBeFalsy())
+        )
     }
 )
 
 describe(
-    "any",
+    "all_varargs",
+    () => {
+        describe(
+            "boolean",
+            () => {
+                test("empty", () => expect(all()).toBeTruthy())
+                test("all true", () => expect(all(1 === 1, true)).toBeTruthy())
+                test("contains false", () => expect(all(1 === 1, false, true)).toBeFalsy())
+                test("all false", () => expect(all(false, 1 > 5)).toBeFalsy())
+            }
+        )
+        describe(
+            "function/method returns boolean",
+            () => {
+                test("empty", () => expect(all()).toBeTruthy())
+                test("all true", () => expect(all(() => 1 === 1, () => true)).toBeTruthy())
+                test("contains false", () => expect(all(() => 1 === 1, () => false, () => true)).toBeFalsy())
+                test("all false", () => expect(all(() => false, () => 1 > 5)).toBeFalsy())
+            }
+        )
+        describe(
+            "mixed",
+            () => {
+                test("empty", () => expect(all()).toBeTruthy())
+                test("all true", () => expect(all(() => 1 === 1, true)).toBeTruthy())
+                test("contains false", () => expect(all(1 === 1, () => false, () => true)).toBeFalsy())
+                test("all false", () => expect(all(() => false, 1 > 5)).toBeFalsy())
+            }
+        )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    all(
+                        true,
+                        false,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ))
+                    .toBeFalsy())
+        )
+    }
+)
+
+describe(
+    "any_array",
     () => {
         describe(
             "boolean",
             () => {
                 test("empty", () => expect(any([])).toBeFalsy())
+                test("all true", () => expect(any([1 === 1, true])).toBeTruthy())
                 test("all true", () => expect(any([1 === 1, true])).toBeTruthy())
                 test("contains false", () => expect(any([1 === 1, false, true])).toBeTruthy())
                 test("all false", () => expect(any([false, 1 > 5])).toBeFalsy())
@@ -73,5 +129,265 @@ describe(
                 test("all false", () => expect(any([() => false, 1 > 5])).toBeFalsy())
             }
         )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    any([
+                        true,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ]))
+                    .toBeTruthy())
+        )
     }
 )
+
+describe(
+    "any_varargs",
+    () => {
+        describe(
+            "boolean",
+            () => {
+                test("empty", () => expect(any()).toBeFalsy())
+                test("all true", () => expect(any(1 === 1, true)).toBeTruthy())
+                test("all true", () => expect(any(1 === 1, true)).toBeTruthy())
+                test("contains false", () => expect(any(1 === 1, false, true)).toBeTruthy())
+                test("all false", () => expect(any(false, 1 > 5)).toBeFalsy())
+            }
+        )
+        describe(
+            "function/method returns boolean",
+            () => {
+                test("empty", () => expect(any()).toBeFalsy())
+                test("all true", () => expect(any(() => 1 === 1, () => true)).toBeTruthy())
+                test("contains false", () => expect(any(() => 1 === 1, () => false, () => true)).toBeTruthy())
+                test("all false", () => expect(any(() => false, () => 1 > 5)).toBeFalsy())
+            }
+        )
+        describe(
+            "mixed",
+            () => {
+                test("empty", () => expect(any()).toBeFalsy())
+                test("all true", () => expect(any(() => 1 === 1, true)).toBeTruthy())
+                test("contains false", () => expect(any(1 === 1, () => false, () => true)).toBeTruthy())
+                test("all false", () => expect(any(() => false, 1 > 5)).toBeFalsy())
+            }
+        )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    any(
+                        true,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ))
+                    .toBeTruthy())
+        )
+    }
+)
+
+describe(
+    "none_array",
+    () => {
+        describe(
+            "boolean",
+            () => {
+                test("empty", () => expect(none([])).toBeTruthy())
+                test("all true", () => expect(none([1 === 1, true])).toBeFalsy())
+                test("all true", () => expect(none([1 === 1, true])).toBeFalsy())
+                test("contains false", () => expect(none([1 === 1, false, true])).toBeFalsy())
+                test("all false", () => expect(none([false, 1 > 5])).toBeTruthy())
+            }
+        )
+        describe(
+            "function/method returns boolean",
+            () => {
+                test("empty", () => expect(none([])).toBeTruthy())
+                test("all true", () => expect(none([() => 1 === 1, () => true])).toBeFalsy())
+                test("contains false", () => expect(none([() => 1 === 1, () => false, () => true])).toBeFalsy())
+                test("all false", () => expect(none([() => false, () => 1 > 5])).toBeTruthy())
+            }
+        )
+        describe(
+            "mixed",
+            () => {
+                test("empty", () => expect(none([])).toBeTruthy())
+                test("all true", () => expect(none([() => 1 === 1, true])).toBeFalsy())
+                test("contains false", () => expect(none([1 === 1, () => false, () => true])).toBeFalsy())
+                test("all false", () => expect(none([() => false, 1 > 5])).toBeTruthy())
+            }
+        )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    none([
+                        true,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ]))
+                    .toBeFalsy())
+        )
+    }
+)
+
+describe(
+    "none_varargs",
+    () => {
+        describe(
+            "boolean",
+            () => {
+                test("empty", () => expect(none()).toBeTruthy())
+                test("all true", () => expect(none(1 === 1, true)).toBeFalsy())
+                test("all true", () => expect(none(1 === 1, true)).toBeFalsy())
+                test("contains false", () => expect(none(1 === 1, false, true)).toBeFalsy())
+                test("all false", () => expect(none(false, 1 > 5)).toBeTruthy())
+            }
+        )
+        describe(
+            "function/method returns boolean",
+            () => {
+                test("empty", () => expect(none()).toBeTruthy())
+                test("all true", () => expect(none(() => 1 === 1, () => true)).toBeFalsy())
+                test("contains false", () => expect(none(() => 1 === 1, () => false, () => true)).toBeFalsy())
+                test("all false", () => expect(none(() => false, () => 1 > 5)).toBeTruthy())
+            }
+        )
+        describe(
+            "mixed",
+            () => {
+                test("empty", () => expect(none()).toBeTruthy())
+                test("all true", () => expect(none(() => 1 === 1, true)).toBeFalsy())
+                test("contains false", () => expect(none(1 === 1, () => false, () => true)).toBeFalsy())
+                test("all false", () => expect(none(() => false, 1 > 5)).toBeTruthy())
+            }
+        )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    none(
+                        true,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ))
+                    .toBeFalsy())
+        )
+    }
+)
+
+describe(
+    "notAll_array",
+    () => {
+        describe(
+            "boolean",
+            () => {
+                test("empty", () => expect(notAll([])).toBeFalsy())
+                test("notAll true", () => expect(notAll([1 === 1, true])).toBeFalsy())
+                test("contains false", () => expect(notAll([1 === 1, false, true])).toBeTruthy())
+                test("notAll false", () => expect(notAll([false, 1 > 5])).toBeTruthy())
+            }
+        )
+        describe(
+            "function/method returns boolean",
+            () => {
+                test("empty", () => expect(notAll([])).toBeFalsy())
+                test("notAll true", () => expect(notAll([() => 1 === 1, () => true])).toBeFalsy())
+                test("contains false", () => expect(notAll([() => 1 === 1, () => false, () => true])).toBeTruthy())
+                test("notAll false", () => expect(notAll([() => false, () => 1 > 5])).toBeTruthy())
+            }
+        )
+        describe(
+            "mixed",
+            () => {
+                test("empty", () => expect(notAll([])).toBeFalsy())
+                test("notAll true", () => expect(notAll([() => 1 === 1, true])).toBeFalsy())
+                test("contains false", () => expect(notAll([1 === 1, () => false, () => true])).toBeTruthy())
+                test("notAll false", () => expect(notAll([() => false, 1 > 5])).toBeTruthy())
+            }
+        )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    notAll([
+                        true,
+                        false,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ]))
+                    .toBeTruthy())
+        )
+    }
+)
+
+describe(
+    "notAll_varargs",
+    () => {
+        describe(
+            "boolean",
+            () => {
+                test("empty", () => expect(notAll()).toBeFalsy())
+                test("notAll true", () => expect(notAll(1 === 1, true)).toBeFalsy())
+                test("contains false", () => expect(notAll(1 === 1, false, true)).toBeTruthy())
+                test("notAll false", () => expect(notAll(false, 1 > 5)).toBeTruthy())
+            }
+        )
+        describe(
+            "function/method returns boolean",
+            () => {
+                test("empty", () => expect(notAll()).toBeFalsy())
+                test("notAll true", () => expect(notAll(() => 1 === 1, () => true)).toBeFalsy())
+                test("contains false", () => expect(notAll(() => 1 === 1, () => false, () => true)).toBeTruthy())
+                test("notAll false", () => expect(notAll(() => false, () => 1 > 5)).toBeTruthy())
+            }
+        )
+        describe(
+            "mixed",
+            () => {
+                test("empty", () => expect(notAll()).toBeFalsy())
+                test("notAll true", () => expect(notAll(() => 1 === 1, true)).toBeFalsy())
+                test("contains false", () => expect(notAll(1 === 1, () => false, () => true)).toBeTruthy())
+                test("notAll false", () => expect(notAll(() => false, 1 > 5)).toBeTruthy())
+            }
+        )
+        describe(
+            "lazy evaluation",
+            () => test("doesn't evaluate non-needed value", () =>
+                expect(
+                    notAll(
+                        true,
+                        false,
+                        () => { throw new Error("unexpected evaluation"); }
+                    ))
+                    .toBeTruthy())
+        )
+    }
+)
+
+describe(
+    "all_any_notAll_none_combinated",
+    () => {
+        test("true", () => expect(
+            all(
+                true,
+                any(
+                    () => false,
+                    notAll(true, () => false)
+                ),
+                none(
+                    false,
+                    () => false
+                )
+            )).toBeTruthy());
+        test("false", () => expect(
+            all(
+                true,
+                any(
+                    () => false,
+                    notAll(true, () => false)
+                ),
+                none(
+                    false,
+                    () => true
+                )
+            )).toBeFalsy());
+    }
+);
